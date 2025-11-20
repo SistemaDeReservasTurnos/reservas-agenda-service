e5c5ae92242da70aefdbc21f1c0152dcba32c837
Mvc2004 <merly.velasquez@correounivalle.edu.co>
2025-11-19 20:19:37 -0500

    archivo de filtro

---
+++ reservas-agenda/src/main/java/com/servicio/reservas/agenda/application/dto/FilterReservationUser.java
@@ -0,0 +1,27 @@
+package com.servicio.reservas.agenda.application.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+public class FilterReservationUser {
+
+    private Long userId;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String status;
+
+    public FilterReservationUser() {}
+
+    public FilterReservationUser(Long userId, LocalDate startDate, LocalDate endDate, String status) {
+        this.userId = userId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+    }
+
+}
+
