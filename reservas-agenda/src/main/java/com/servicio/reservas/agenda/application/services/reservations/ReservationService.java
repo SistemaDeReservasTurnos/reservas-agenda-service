08fdb56cf0885789d41c76c38ee39c9b63886196
Nicolas Rodriguez <nicolas.rodriguez.romero@correounivalle.edu.co>
2025-12-05 00:08:02 -0500

    comunicación con usuarios y propagación de token

--- reservas-agenda/src/main/java/com/servicio/reservas/agenda/application/services/reservations/ReservationService.java
+++ reservas-agenda/src/main/java/com/servicio/reservas/agenda/application/services/reservations/ReservationService.java
@@ -12,6 +12,9 @@ import com.servicio.reservas.agenda.domain.repository.IReservationRepository;
 import com.servicio.reservas.agenda.infraestructure.exception.BusinessException;
 import com.servicio.reservas.agenda.infraestructure.exception.ResourceNotFoundException;
 import com.servicio.reservas.agenda.infraestructure.services.ServiceDTO;
+import com.servicio.reservas.agenda.infraestructure.users.UserClient;
+import com.servicio.reservas.agenda.infraestructure.users.UserDTO;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -23,10 +26,12 @@ public class ReservationService implements IReservationService {
 
     private final IShiftService shiftService;
     private final IReservationRepository  reservationRepository;
+    private final UserClient userClient;
 
-    public ReservationService(IShiftService shiftService, IReservationRepository reservationRepository) {
+    public ReservationService(IShiftService shiftService, IReservationRepository reservationRepository, UserClient userClient) {
         this.shiftService = shiftService;
         this.reservationRepository = reservationRepository;
+        this.userClient = userClient;
     }
 
     @Override
@@ -50,7 +55,7 @@ public class ReservationService implements IReservationService {
 
         shiftService.createShift(reservation2);
 
-        return ReservationMapper.toResponse(reservation2);
+        return buildResponseWithUserNames(reservation2);
     }
 
     @Override
@@ -78,7 +83,7 @@ public class ReservationService implements IReservationService {
             foundReservation.updateReservation(reservation.getDate(), reservation.getTimeStart(), endTime);
             Reservation updatedR = reservationRepository.save(foundReservation);
 
-            return ReservationMapper.toResponse(updatedR);
+            return buildResponseWithUserNames(updatedR);
 
         }else {
             throw new BusinessException("The reservation ID " + id + " is deactivated.");
@@ -131,21 +136,21 @@ public class ReservationService implements IReservationService {
         );
 
         // Convertir a DTO
-        return results.stream().map(ReservationMapper::toResponse).toList();
+        return results.stream().map(this::buildResponseWithUserNames).toList();
     }
 
     @Override
     public List<ResponseReservation> searchAllReservationsAdmin(FilterReservationAdmin filters) {
 
         List<Reservation> list = reservationRepository.adminSearchReservations(filters);
-        return list.stream().map(ReservationMapper::toResponse).toList();
+        return list.stream().map(this::buildResponseWithUserNames).toList();
     }
 
     @Override
     public ResponseReservation findReservationById(Long id) {
 
         Reservation reservation = findReservationByIdInternal(id);
-        return ReservationMapper.toResponse(reservation);
+        return buildResponseWithUserNames(reservation);
     }
 
     private Reservation findReservationByIdInternal(Long id) {
@@ -157,5 +162,20 @@ public class ReservationService implements IReservationService {
             throw new BusinessException("The reservation ID " + id + " is deactivated.");
         }
         return reservation;
+    }
+
+    private ResponseReservation buildResponseWithUserNames(Reservation reservation) {
+
+        UserDTO client = userClient.findUserById(reservation.getUserId())
+                .orElseThrow(() -> new ResourceNotFoundException("The client is not found."));
+
+        UserDTO barber = userClient.findUserById(reservation.getBarberId())
+                .orElseThrow(() -> new ResourceNotFoundException("The barber is not found."));
+
+        return ReservationMapper.toResponse(
+                reservation,
+                client.getName(),
+                barber.getName()
+        );
     }
 }
